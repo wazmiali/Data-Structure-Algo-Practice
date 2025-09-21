cb9633ae1c6c1b362688acb3977d763f17943d70
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2025-09-21 08:48:32 +0530

    Update PointerArray.cpp

--- DSA C++/Lecture 26 Pointer Array/PointerArray.cpp
+++ DSA C++/Lecture 26 Pointer Array/PointerArray.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 int main()
 {
+      // Array Index size is 10
       int arr[10] = {0, 1};
 
       cout <<"address of first memory block is "<< arr << endl;
       cout << arr[1] <<endl;
       cout <<"address of first memory block is "<< &arr[0] << endl;
 
-
-
       return 0;
+
 }
