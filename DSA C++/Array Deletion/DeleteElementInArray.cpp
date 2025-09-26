22cac2a2f5efdf22d2e1686b7f4cd4daee06b1a6
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2025-09-26 20:58:03 +0530

    Update DeleteElementInArray.cpp
    // Array Deletion or Insert and sort or Update 
    // Delete 
    // Insert
    // Update
    // Sort

--- DSA C++/Array Deletion/DeleteElementInArray.cpp
+++ DSA C++/Array Deletion/DeleteElementInArray.cpp
@@ -1,4 +1,8 @@
-// Array Deletion or Insert and sort 
+// Array Deletion or Insert and sort or Update 
+// Delete 
+// Insert
+// Update
+// Sort
 
 #include <iostream>
 #include <vector>
@@ -14,13 +18,20 @@ int main()
             cout<< x << " ";
       cout<<endl;
 
+      // Delete
       v.erase(v.begin() + 3); // delete element of index of 3
 
+      // Inser
       v.insert(v.begin() + 4, 22); // Insert a new element in the position id index 4
 
+      // Update
+      v[1] = 0; // update a Array 
+
+      // Sort
       sort(v.begin(), v.end()); // Sort a array in ascending order
 
       cout << "After Deletion of a Array: ";
       for (int x : v)
             cout << x << " ";
+
 }
