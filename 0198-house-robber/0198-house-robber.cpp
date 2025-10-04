9802e6a3691b236fc8b405e0895e9e70a27505e4
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2025-10-04 09:11:14 +0530

    Time: 0 ms (100%), Space: 10.8 MB (5.82%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,20 @@
+// Using Dp
+class Solution {
+public:
+    int solve(vector<int>& nums, int size, int index, vector<int>& dp) {
+        if (index >= size) return 0;
+
+        if (dp[index] != -1) return dp[index];
+
+        int option1 = nums[index] + solve(nums, size, index + 2, dp);
+        int option2 = solve(nums, size, index + 1, dp);
+
+        return dp[index] = max(option1, option2);
+    }
+
+    int rob(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> dp(size, -1);
+        return solve(nums, size, 0, dp);
+    }
+};
