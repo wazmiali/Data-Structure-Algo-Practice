13f6f2550f31b24b8222a2bba2232fc95bdfc6ea
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2025-11-03 20:36:48 +0530

    Time: 0 ms (100%), Space: 7.7 MB (96.98%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    // Helper function to calculate sum of squares of digits
+    int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            totalSum += d * d;
+            n /= 10;
+        }
+        return totalSum;
+    }
+
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = getNext(n);
+        
+        while (fast != 1 && slow != fast) {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        }
+
+        return fast == 1;
+    }
+};
