484fd24a304bc09e279aa115cf0b9ef9b718d6e1
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2025-11-03 20:28:01 +0530

    Time: 0 ms (100%), Space: 15.3 MB (83.22%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
@@ -0,0 +1,16 @@
+int maxProfit(int* prices, int pricesSize) {
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 1; i < pricesSize; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else {
+            int profit = prices[i] - minPrice; 
+            if (profit > maxProfit)
+                maxProfit = profit; 
+        }
+    }
+
+    return maxProfit;
+}
