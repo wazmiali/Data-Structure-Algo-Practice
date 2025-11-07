ba4815bf3468e5d67f800322f054b5ffd9488b4e
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2025-11-07 12:12:42 +0530

    0/1 Knapsack Problem Algorithm

---
+++ DAA Lab/KnapSackProblem2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int knapSack(int W, int wt[], int val[], int n) {
+    int k[n + 1][W + 1];  
+
+    for (int i = 0; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+
+            if (i == 0 || w == 0)
+                k[i][w] = 0;
+
+            else if (wt[i - 1] <= w)
+                k[i][w] = max(
+                    val[i - 1] + k[i - 1][w - wt[i - 1]],
+                    k[i - 1][w]
+                );
+
+            else
+                k[i][w] = k[i - 1][w];
+        }
+    }
+
+    return k[n][W]; 
+}
+
+int main() {
+    int val[] = {60, 100, 120};
+    int wt[] = {10, 20, 30};
+    int W = 50;
+    int n = sizeof(val) / sizeof(val[0]);
+
+    printf("Maximum value in knapsack = %d\n", knapSack(W, wt, val, n));
+    return 0;
+}
