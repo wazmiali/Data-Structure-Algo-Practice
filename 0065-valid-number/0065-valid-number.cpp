e02f326504de845cc2d5597c4de45db5beefc03f
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2025-11-10 20:17:36 +0530

    Time: 0 ms (100%), Space: 8.2 MB (61.56%) - LeetHub

---
+++ 0065-valid-number/0065-valid-number.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isNumber(string s) {
+        bool num = false;
+        bool exp = false;
+        bool dot = false;
+        
+        for(int i = 0; i < s.size(); i++){
+            char c = s[i];
+            
+            if(isdigit(c)){
+                num = true;
+            }
+            else if(c == '.'){
+                if(dot || exp) return false;
+                dot = true;
+            }
+            else if(c == 'e' || c == 'E'){
+                if(exp || !num) return false;
+                exp = true;
+                num = false;
+            }
+            else if(c == '+' || c == '-'){
+                if(i != 0 && s[i-1] != 'e' && s[i-1] != 'E')
+                    return false;
+            }
+            else{
+                return false;
+            }
+        }
+        
+        return num;
+    }
+};
