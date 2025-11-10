cc510d1dd53f75469d5e98f8bb097272cd12877e
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2025-11-10 22:35:17 +0530

    Update 0088-merge-sorted-array.cpp

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -6,7 +6,7 @@ public:
         int p1 = m - 1;          
         int p2 = n - 1;         
         int p = m + n - 1;       
-
+// check
         while (p1 >= 0 && p2 >= 0) {
             if (nums1[p1] > nums2[p2]) {
                 nums1[p] = nums1[p1];
