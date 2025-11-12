0fa76f72a9fe71af01ba85d4091ae30c1f9793c4
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2025-11-12 21:56:50 +0530

    Time: 0 ms (100%), Space: 8.6 MB (68.2%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,18 +1,18 @@
-//Palindrome Number 
 class Solution {
 public:
     bool isPalindrome(int x) {
+    
         if (x < 0) return false;
 
-        long long sum = 0;
+        long long reversed = 0;
         int original = x;
 
         while (x != 0) {
-            int rem = x % 10;
-            sum = sum * 10 + rem;
-            x /= 10;
+            int digit = x % 10;         
+            reversed = reversed * 10 + digit; 
+            x /= 10;                   
         }
 
-        return sum == original;
+        return reversed == original;
     }
 };
