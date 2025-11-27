3550757792d89c96e959b8d6cec1b8d5e2bce2d9
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2025-11-27 22:07:53 +0530

    Time: 0 ms (100%), Space: 16.9 MB (5.01%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,14 +1,40 @@
 class Solution {
+    private: 
+        void solve(vector<int> nums, vector<int> output, int index, vector<vector<int> >& ans){
+            //base case
+            if(index >= nums.size()){
+                ans.push_back(output);
+                return;
+            }
+
+            // exclude 
+            solve(nums, output, index+1, ans);
+
+            // include
+            int element = nums[index];
+            output.push_back(element);
+            solve(nums, output, index+1, ans);
+        }
+
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> subs = {{}};
-        for (int num : nums) {
-            int n = subs.size();
-            for (int i = 0; i < n; i++) {
-                subs.push_back(subs[i]); 
-                subs.back().push_back(num);
-            }
-        }
-        return subs;
+
+        vector<vector<int> > ans;
+        vector<int> output;
+        int index = 0;
+        solve(nums, output, index, ans);
+
+        return ans;
+
+
+        // vector<vector<int>> subs = {{}};
+        // for (int num : nums) {
+        //     int n = subs.size();
+        //     for (int i = 0; i < n; i++) {
+        //         subs.push_back(subs[i]); 
+        //         subs.back().push_back(num);
+        //     }
+        // }
+        // return subs;
     }
 }; 
