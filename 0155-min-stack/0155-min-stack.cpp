b3e67fd2e228b67dc68eca1fa061a896eeb181f8
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2026-03-05 22:35:27 +0530

    Time: 0 ms (100%), Space: 23.4 MB (49.57%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,14 +1,14 @@
-class MinStack{
+class MinStack {
     stack<int> s, st;
 public:
-    void push(int val){
+    void push(int val) {
         s.push(val);
-        if(st.empty() || val <= st.top()) st.push(val);
+        if (st.empty() || val <= st.top()) st.push(val);
     }
-    void pop(){
-        if(st.top() == st.top()) st.pop();
+    void pop() {
+        if (s.top() == st.top()) st.pop();
         s.pop();
     }
-    int top(){ return s.empty() ? -1 : s.top(); }
-    int getMin(){ return s.empty() ? -1 : st.top(); }
+    int top() { return s.empty() ? -1 : s.top(); }
+    int getMin() { return st.empty() ? -1 : st.top(); }
 };
