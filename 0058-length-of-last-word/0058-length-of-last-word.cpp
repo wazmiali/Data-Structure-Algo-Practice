42232d2af3bb4220b6ffb78de78eefb849795658
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2026-03-10 21:37:05 +0530

    Time: 0 ms (100%), Space: 9 MB (32.82%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int n = s.length();
+
         int len = 0;
 
         int i = n - 1;
-        
-        while (i >= 0 && s[i] == ' ') {
+        while(i >= 0 && s[i] == ' '){
             i--;
         }
-        
-        while (i >= 0 && s[i] != ' ') {
+        while(i >= 0 && s[i] != ' '){ 
             len++;
             i--;
         }
         
         return len;
+
+        
+ 
+       
     }
 };
