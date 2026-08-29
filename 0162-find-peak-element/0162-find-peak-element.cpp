dffaaaba07967cc880269fea619d0a65d8595f6a
Wazmi Ali <113008194+wazmiali@users.noreply.github.com>
2026-08-29 21:31:08 +0530

    Time: 0 ms (100%), Space: 12.6 MB (46.99%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,38 @@
+// class Solution {
+// public:
+//     int findPeakElement(vector<int>& nums) {
+
+//         int n=nums.size();
+//         int start=0;
+//         int end=n-1;
+//         while(start<=end){
+//             int mid=start+(end-start)/2;
+//             if(mid>=0 && mid<n-1 && nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
+//             else if(mid<n-1 && nums[mid]<nums[mid+1]) start=mid+1;
+//             else end=mid-1;
+//         }
+//         if(n!=1 && nums[0]>nums[1]) return 0;
+//         else if(n!=1 && nums[n-1]> nums[n-2]) return n-1;
+        
+//         return 0;
+//     }
+// };
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        int start=0;
+        int end=n-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(mid>0 && mid<n-1 && nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
+            else if(mid<n-1 && nums[mid]<nums[mid+1]) start=mid+1;
+            else end=mid-1;
+        }
+        if(n!=1 && nums[0]>nums[1] ) return 0;
+        else if(n!=1 && nums[n-1]>nums[n-2]) return n-1;
+
+        return 0;
+    }
+};
